29420c0397cf44b18b50adb12d6c68061ce926d2
Tantatorn <timefear007@gmail.com>
2019-12-18 20:59:50 +0700

    try react vis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import { XYPlot,VerticalBarSeries,MarkSeries, LineSeries } from "react-vis";
 
 function App() {
+  const data = [
+    { x: 0, y: 8 },
+    { x: 1, y: 5 },
+    { x: 2, y: 4 },
+    { x: 3, y: 9 },
+    { x: 4, y: 1 },
+    { x: 5, y: 7 },
+    { x: 6, y: 6 },
+    { x: 7, y: 3 },
+    { x: 8, y: 2 },
+    { x: 9, y: 0 }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <XYPlot height={200} width={200}>
+        <VerticalBarSeries data={data} />
+      </XYPlot>
+      <XYPlot height={200} width={200}>
+        <LineSeries data={data} />
+      </XYPlot>
+      <XYPlot height={200} width={200}>
+        <MarkSeries data={data} />
+      </XYPlot>
     </div>
   );
 }
