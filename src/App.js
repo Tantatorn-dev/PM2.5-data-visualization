3b0454493db79540ac75c753a26154b57a3334dd
Tantatorn <timefear007@gmail.com>
2020-01-05 16:25:06 +0700

    change to scatterplot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import '../node_modules/react-vis/dist/style.css';
-import { XYPlot, LineSeries, XAxis, YAxis } from "react-vis";
+import { XYPlot, MarkSeries, XAxis, YAxis } from "react-vis";
 import extractData from "./lib/data";
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
@@ -9,15 +9,15 @@ import "react-datepicker/dist/react-datepicker.css";
 function App() {
   const [data, setData] = useState([])
   const [isLoading, toggleIsLoading] = useState(true)
-  const [startDate, setStartDate] = useState(new Date("2014/02/08"))
-  const [endDate, setEndDate] = useState(new Date("2014/03/08"))
+  const [startDate, setStartDate] = useState(new Date("2019/12/17"))
+  const [endDate, setEndDate] = useState(new Date("2020/01/04"))
 
   const getData = () => {
     toggleIsLoading(true)
     fetch("https://tgr2020-quiz2.firebaseio.com/quiz/sensor/team32.json")
       .then(res => res.json())
       .then(res => {
-        extractData(res, function (xData, yData) {
+        extractData(res,startDate,endDate, function (xData, yData) {
           let temp = []
           xData.map((key, i) => {
             temp.push({ x: xData[i], y: yData[i] })
@@ -60,7 +60,7 @@ function App() {
           (<XYPlot xType="time" height={500} width={1000}>
             <XAxis title="Time" />
             <YAxis title="PM2.5 sensor value" />
-            <LineSeries data={data} />
+            <MarkSeries data={data} size={1}/>
           </XYPlot>) : <h1>loading ...</h1>
       }
     </div>
