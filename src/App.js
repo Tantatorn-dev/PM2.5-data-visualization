1540fb502a7be99e56dbcca8b554ff7ce5cb437f
Tantatorn <timefear007@gmail.com>
2020-01-05 12:02:48 +0700

    add loading screen

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import extractData from "./lib/data";
 
 function App() {
   const [data, setData] = useState([])
+  const [isLoading, toggleIsLoading] = useState(true)
 
   const getData = () => {
+    toggleIsLoading(true)
     fetch("https://tgr2020-quiz2.firebaseio.com/quiz/sensor/team32.json")
       .then(res => res.json())
       .then(res => {
-        extractData(res,function(xData,yData) {
+        extractData(res, function (xData, yData) {
           let temp = []
-          xData.map((key,i) => {
+          xData.map((key, i) => {
             temp.push({ x: xData[i], y: yData[i] })
           })
           setData(temp)
+          toggleIsLoading(false)
         })
       })
       .catch((error) => { console.log(error) })
@@ -27,11 +30,14 @@ function App() {
     <div className="App">
       <h1>PM2.5 sensor data visualization</h1>
       <button onClick={getData}>fetch data</button>
-      <XYPlot xType="time" height={500} width={1000}>
-        <XAxis title="Time"/>
-        <YAxis title="PM2.5 sensor value"/>
-        <LineSeries data={data} />
-      </XYPlot>
+      {
+        !isLoading ?
+        (<XYPlot xType="time" height={500} width={1000}>
+          <XAxis title="Time" />
+          <YAxis title="PM2.5 sensor value" />
+          <LineSeries data={data} />
+        </XYPlot>):<h1>loading ...</h1>
+      }
     </div>
   );
 }
